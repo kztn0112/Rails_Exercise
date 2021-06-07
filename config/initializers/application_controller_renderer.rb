7016f2062054c11a659c55d17abfc3847728b4f9
Hiroaki Ninomiya <ec2-user@ip-172-31-73-68.ec2.internal>
2021-06-07 02:46:35 +0000

    [Finish]Rails-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '83e06a86a2f0628505d6fff8353c53bc9371f88cce660973b5cc703d02e61f594af3087db3a4bda308538b6abcd316ef093e2ff0ab8ba6036de3671874c53151'
